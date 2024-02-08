479bb75a5cc5c8a9548f0eda199933e217d3b881
Souvik De <souvik.de@sato-global.com>
2024-02-08 12:48:15 +0900

    leetcode: day 3 - add Kids With the Greatest Number of Candies

---
+++ Day3/GreatestNumberofCandies.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    var output = []
+    var maxCandies = Math.max(...candies)
+    for(var i=0;i<candies.length;i++){
+        if(candies[i]+extraCandies >= maxCandies){
+            output.push(true)
+        }
+        else{
+            output.push(false)
+        }
+    }
+    return output
+};
