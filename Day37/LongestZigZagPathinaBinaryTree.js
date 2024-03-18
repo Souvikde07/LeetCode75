43ce670f7a5aca9e354c661a3bd17350774996e3
Souvik De <souvik.de@sato-global.com>
2024-03-18 15:48:18 +0900

    leetcode: day 38 - add Lowest Common Ancestor of a Binary Tree

--- Day37/LongestZigZagPathinaBinaryTree.js
+++ Day37/LongestZigZagPathinaBinaryTree.js
@@ -56,3 +56,16 @@ class Solution {
         }
     };
     */
+var longestZigZag = function(root) {
+    let ans = 0;
+    const stack = [[root, 0, null]];
+    while (stack.length > 0) {
+        const [node, n, left] = stack.pop();
+        if (node) {
+            ans = Math.max(ans, n);
+            stack.push([node.left, left != null && left == 0 ? n + 1 : 1, 1]);
+            stack.push([node.right, left != null && left == 1 ? n + 1 : 1, 0]);
+        }
+    }
+    return ans;
+};
