69c7ffb5147be30cbcd3e2896c98ee17729fcf05
Souvik De <souvik.de@sato-global.com>
2024-03-25 10:57:16 +0900

    leetcode: day 45 - add Reorder Routes to Make All Paths Lead to the City Zero

--- Day44/NumberofProvinces.cpp
+++ Day44/NumberofProvinces.cpp
@@ -3,7 +3,7 @@ class Solution {
 public:
     void dfs(unordered_map<int, vector<int>> &adj, int u, vector<bool>& visited) {
         visited[u] = true;
-        
+
         //Visit neighbours
         for(int &v : adj[u]) {
             if(!visited[v]) {
@@ -11,13 +11,12 @@ public:
             }
         }
     }
-    
-    
+
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        
+
         unordered_map<int, vector<int>> adj;
-        
+
         for(int i = 0; i<n; i++) {
             for(int j = 0; j<n; j++) {
                 if(isConnected[i][j] == 1) {
@@ -26,18 +25,50 @@ public:
                 }
             }
         }
-        
+
         vector<bool> visited(n, false);
         int count = 0;
-        
+
         for(int i = 0; i<n; i++) {
             if(!visited[i]) {
                 count++;
                 dfs(adj, i, visited);
             }
         }
-        
+
         return count;
-        
+
+    }
+};
+//approach-2
+class Solution {
+public:
+    int n;
+    void dfs(vector<vector<int>> &adj, int u, vector<bool>& visited) {
+        visited[u] = true;
+
+        //Visit neighbours
+        for(int v = 0; v < n; v++) {
+            if(adj[u][v] == 1 && !visited[v]) {
+                dfs(adj, v, visited);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        n = isConnected.size();
+
+        vector<bool> visited(n, false);
+        int count = 0;
+
+        for(int i = 0; i<n; i++) {
+            if(!visited[i]) {
+                count++;
+                dfs(isConnected, i, visited);
+            }
+        }
+
+        return count;
+
     }
 };
